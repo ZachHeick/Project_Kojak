86bf70cc264f250ce0580286ee4db5a8ccc8ad2f
Zach Heick <zheick9595@gmail.com>
2017-12-14 16:30:42 -0600

    Comments

--- Web_App/web_app.py
+++ Web_App/web_app.py
@@ -11,8 +11,10 @@ app = flask.Flask(__name__)
 
 POSTGRESQL_U = str(os.environ['zU'])
 POSTGRESQL_P = str(os.environ['zP'])
+AWS_PROJECT_KOJAK_EC2 = str(os.environ['AWS_PROJECT_KOJAK_EC2'])
 
-DB_URL = 'postgresql://' + POSTGRESQL_U + ':' + POSTGRESQL_P + '@13.59.54.149:5432/project_kojak'
+# Connect to database
+DB_URL = 'postgresql://' + POSTGRESQL_U + ':' + POSTGRESQL_P + AWS_PROJECT_KOJAK_EC2
 engine = create_engine(DB_URL)
 
 df = pd.read_sql_query('''SELECT 
@@ -28,14 +30,17 @@ df = pd.read_sql_query('''SELECT
                            JOIN track_previews ON track_previews.track_id = tracks.track_id
                            WHERE tracks.energy IS NOT NULL
                            AND lyrics.lyrics IS NOT NULL
+                           AND artists.artist_name != 'Kid Rock'
                            ORDER BY tracks.track_name;''', engine)
 
+# Create dataframe
 df['track_name'] = df['track_name'].apply(lambda i: string.capwords(i))
 df.drop_duplicates(subset=['track_name', 'artist_name'], inplace=True)
 df.reset_index(drop=True, inplace=True)
 df.drop(df[df['lyrics'].str.contains('<span')].index, inplace=True)
 df.reset_index(drop=True, inplace=True)
 
+# Lookup table for tracks and artists where keys are "Track by Artist" and value is an index
 TRACKS_AND_ARTISTS = []
 TRACKS_AND_ARTIST_INDEXES = {}
 for i, row in enumerate(df[['track_name', 'artist_name']].itertuples()):
@@ -50,18 +55,26 @@ with open('../Data_Files/neighbors', 'rb') as f:
 
 @app.route('/track_preview', methods=['POST'])
 def play_track_preview():
+    """
+    Gets track audio preview
+    :return: jsonified audio preview
+    """
     data = flask.request.get_json()
     track_and_artist = data['track_and_artist']
     index = TRACKS_AND_ARTIST_INDEXES[track_and_artist]
     track_preview_data = {}
     track = df.iloc[index]
-    print(track['track_preview_url'])
     track_preview_data = {'preview_url': track['track_preview_url']}
 
     return flask.jsonify(track_preview_data)
 
+
 @app.route('/track_info', methods=['POST'])
 def track_info():
+    """
+    Gets basic track data
+    :return: jsonified track data
+    """
     data = flask.request.get_json()
     track_and_artist = data['track_and_artist']
     index = TRACKS_AND_ARTIST_INDEXES[track_and_artist]
@@ -79,6 +92,10 @@ def track_info():
 
 @app.route('/playlist', methods=['POST'])
 def playlist():
+    """
+    Gets the playlist
+    :return: jsonified playlist
+    """
     data = flask.request.get_json()
     track_and_artist = data['track_and_artist']
     index = TRACKS_AND_ARTIST_INDEXES[track_and_artist]
@@ -95,6 +112,10 @@ def playlist():
 
 @app.route('/autocomplete', methods=['GET'])
 def autocomplete():
+    """
+    jquery autocomplete available songs
+    :return: jsonified tracks and artists
+    """
     return flask.jsonify(json_list=TRACKS_AND_ARTISTS)
 
 
